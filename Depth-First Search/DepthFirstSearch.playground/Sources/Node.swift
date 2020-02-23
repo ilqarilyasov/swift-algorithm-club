5c55535b1e6b7bc91b1756bdbc70505449b2e72e
Ilgar Ilyasov <iilyasov@awarex.com>
2020-02-22 19:02:18 -0500

    Update deprecated index with firstIndex

--- Depth-First Search/DepthFirstSearch.playground/Sources/Node.swift
+++ Depth-First Search/DepthFirstSearch.playground/Sources/Node.swift
@@ -23,7 +23,7 @@ public class Node: CustomStringConvertible, Equatable {
   }
   
   public func remove(_ edge: Edge) {
-    neighbors.remove(at: neighbors.index { $0 === edge }!)
+    neighbors.remove(at: neighbors.firstIndex { $0 === edge }!)
   }
 }
 
